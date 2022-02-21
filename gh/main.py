de75c17cfca24419a3b539ea32e1a6979688f3bc
Tycozr Voo <96928563+TycozrVoo@users.noreply.github.com>
2022-02-20 21:27:47 -0500

    Update main.py

--- gh/main.py
+++ gh/main.py
@@ -2,13 +2,11 @@
 A GitHub info website
 
 Author:
-@FirestarDev (jdl-joseph on GitHub)
+@FirestarDev
 
 Usage:
 For user info: /user_query?user={username}
 For organization info: /org_query?org={organization}
-
-Don't get lost! :3
 """
 
 from flask import Flask, request, render_template
@@ -40,10 +38,5 @@ def uquery():
 @app.route("/org_query", methods=["GET"])
 def oquery():
   return "coming soon"
-
-@app.route("/lost", methods = ["GET"]) 
-def lost():
-  # disable adblocker for this to work
-  return "<iframe src='https://www.youtube.com/embed/Ik_ZPUbnEJs?ecver=1&autoplay=1' width='0' height='0' frameborder='0' allow='autoplay' allowfullscreen></iframe>"
 
 app.run(host="0.0.0.0", port=8080, debug = True)
